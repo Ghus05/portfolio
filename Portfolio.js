dbe7ca09fba65839b9757aac236171e0af4e473c
Ghus05 <86093901+Ghus05@users.noreply.github.com>
2021-08-01 13:31:41 -0400

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,16 @@
+$(function() {
+        $("#nav-close").click(function() {
+            $("#nav-menu").hide();
+        });
+});
+
+$(function() {
+        $("nav-toggle").click(function() {
+            $("#nav-menu").show();
+        });
+});
+
+
+var navMenu = document.getElementById("nav-menu");
+var navToggle = document.getElementById("nav-toggle");
+
